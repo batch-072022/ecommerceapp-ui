f6a27d10c009b7726037d6fb89b5b4f098703138
Sai Dheeraj Hazare <saidheeraj.hazare@valuemomentum.com>
2022-08-05 18:32:40 +0530

    Updating Block user

--- src/app/block-user/block-user.component.ts
+++ src/app/block-user/block-user.component.ts
@@ -16,29 +16,38 @@ export class BlockUserComponent implements OnInit {
 
   useraccess:any;
  getUseraccess(){
-  const url="http://localhost:3000/useraccess";
+  const url="https://ecommerce-apii.herokuapp.com/users";
 
   this.http.get(url).subscribe((res:any)=>{
   this.useraccess = res;
    
   })
  }
-
+ user:any;
  Blocked(user:any){
-  const url="http://localhost:3000/useraccess/"+user.id;
+  const url="https://ecommerce-apii.herokuapp.com/users"+user.id;
 let userObj= {id:user.id,status:"BLOCKED"}
   this.http.patch(url,userObj).subscribe((res:any)=>{
-  this.useraccess = res;
+  //this.useraccess = res;
   alert("User is Blocked");
    
   })
  }
+ getUsers(){
+ const url = "https://ecommerce-apii.herokuapp.com/users";
+    this.http.get(url).subscribe ((res:any)=>{
+      this.user = res;
+    })
+
+  }
+
+ 
 
  Unblocked(user:any){
-  const url="http://localhost:3000/useraccess/"+user.id;
+  const url="https://ecommerce-apii.herokuapp.com/users"+user.id;
   let userObj= {id:user.id,status:"ACTIVE"}
   this.http.patch(url,userObj).subscribe((res:any)=>{
-  this.useraccess = res;
+ // this.useraccess = res;
   alert("User is Un-Blocked");
   })
 
