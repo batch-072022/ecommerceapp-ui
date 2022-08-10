6a31bc4269fc8d61d6a7962b3c39610bed16b08b
Naresh Kumar H <nareshkumarh@live.com>
2022-08-10 15:45:01 +0530

    Fixed ui bugs

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DeleteproductComponent } from './deleteproduct/deleteproduct.component';
+import { ProductRoutingModule } from './product-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DeleteproductComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
