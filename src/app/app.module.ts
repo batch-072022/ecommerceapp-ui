8017635c643f1c15ece464ef9aa4ea0adc593aa3
Vishal Davalapalli <vishal.quartz@gmail.com>
2022-08-10 10:45:22 +0530

    updating view in view user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ViewComponent } from './view/view.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ViewComponent
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
