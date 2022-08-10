81ab27d8adc86a834a7617a465b9d5b55a47d403
Harshil Parlikar <harshil.parlikar@valuemomentum.com>
2022-08-10 12:22:53 +0530

    delete-product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DeleteproductComponent } from './deleteproduct/deleteproduct.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    DeleteproductComponent
   ],
   imports: [
     BrowserModule,
